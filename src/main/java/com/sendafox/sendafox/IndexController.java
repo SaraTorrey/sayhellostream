2b3ee6b8cf3403ead7509db23b9c8f22511dfa8d
Dan Torrey <danielowentorrey@gmail.com>
2018-12-08 17:13:27 -0600

    Add CSS file and map it from HTML file

--- src/main/java/com/sendafox/sendafox/IndexController.java
+++ src/main/java/com/sendafox/sendafox/IndexController.java
@@ -2,13 +2,21 @@ package com.sayhellostream.sayhellostream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping(name = "/")
+    @GetMapping(name = "index")
     public String index() {
 
         return "index";
+    }
+
+    @GetMapping(value = "anotherPage")
+    public String anotherPage() {
+
+        return "anotherPage";
     }
 }
