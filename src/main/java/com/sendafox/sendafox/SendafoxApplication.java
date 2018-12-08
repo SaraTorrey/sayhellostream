3e141e1b87902cd61298d1d96d1e3157644cc8ce
Dan Torrey <danielowentorrey@gmail.com>
2018-12-08 16:39:55 -0600

    initial commit

---
+++ src/main/java/com/sendafox/sendafox/SendafoxApplication.java
@@ -0,0 +1,12 @@
+package com.sayhellostream.sayhellostream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendafoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendafoxApplication.class, args);
+	}
+}
