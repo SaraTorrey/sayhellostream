4f119ebcf56d993bc6f8fbc6077190d8525dbc30
Dan Torrey <dan@graylog.com>
2019-01-06 15:56:25 -0600

    Add missing credential error checking in TwillioSender

--- src/main/java/com/sayhellostream/TwillioClient.java
+++ src/main/java/com/sayhellostream/TwillioClient.java
@@ -3,25 +3,36 @@ package com.sayhellostream;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.apache.commons.lang.StringUtils;
 
 public class TwillioClient {
 
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = System.getenv( "TWILLIO_ACCOUNT_SID" );
-    public static final String AUTH_TOKEN = System.getenv( "TWILLIO_AUTH_TOKEN" );
+    public static String accountSid = System.getenv("TWILLIO_ACCOUNT_SID");
+    public static String authToken = System.getenv("TWILLIO_AUTH_TOKEN");
 
     // Make sure from phone is in format +19254444444
-    public static final String FROM_PHONE = System.getenv( "TWILLIO_FROM_PHONE" );
+    public static String fromPhone = System.getenv("TWILLIO_FROM_PHONE");
 
     public static void send(String to, String from, String body) {
 
-        Twilio.init( ACCOUNT_SID, AUTH_TOKEN);
+        if (configInitialized()) {
 
-        Message message = Message.creator(
-                new PhoneNumber( to),
-                new PhoneNumber(from),
-                body).create();
+            Twilio.init(accountSid, authToken);
 
-        System.out.println(message.getSid());
+            Message message = Message.creator(
+                    new PhoneNumber(to),
+                    new PhoneNumber(from),
+                    body).create();
+
+            System.out.println(message.getSid());
+        } else {
+            System.out.println("Twillio config is not initialized");
+        }
+    }
+
+    private static boolean configInitialized() {
+
+        return StringUtils.isNotBlank(accountSid) && StringUtils.isNotBlank(authToken);
     }
 }
